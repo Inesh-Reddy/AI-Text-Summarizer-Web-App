396dd846a332ea053f426fd6231a034b2b544bcd
Inesh-Reddy <ineshreddy007@gmail.com>
2025-06-23 08:33:01 +0530

    implemented register endpoint

--- backend/src/controllers/user/index.js
+++ backend/src/controllers/user/index.js
@@ -1,7 +1,39 @@
-const register = (req, res) => {
-  res.json({
-    msg: "user register endpoint",
-  });
+const { userModel } = require("../../models/user");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.status(200).json({
+        msg: "User with email already exist",
+      });
+    }
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+      if (err) {
+        return res.status(500).json({
+          msg: "server error",
+          error: err,
+        });
+      }
+      const result = await userModel.create({
+        email,
+        password: hash,
+      });
+
+      res.status(200).json({
+        msg: "user created successfully",
+        User: result,
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Server error",
+      err: error,
+    });
+  }
 };
 
 const login = (req, res) => {
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -4,10 +4,10 @@ const ObjectId = mongoose.Types.ObjectId;
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
-  password: { types: String, required: true },
+  password: { type: String, required: true },
 });
 
-const userModel = new mongoose.model("users", userSchema);
+const userModel = mongoose.model("users", userSchema);
 
 module.exports = {
   userModel,
