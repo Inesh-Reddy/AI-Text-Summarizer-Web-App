c4c2a65c0dc9415f141c0ae4235ed8ab38335aef
Inesh-Reddy <ineshreddy007@gmail.com>
2025-06-24 09:45:02 +0530

    added signup component

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const onSubmitHandler = () => {};
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const username = usernameRef.current.value.trim();
+    const password = passwordRef.current.value.trim();
+    if (!username || !password) {
+      alert("Please fill in both username and password fields");
+      return;
+    }
+
+    const response = await axios.post(
+      "http://localhost:3000/api/v1/user/auth/register",
+      { email: username, password }
+    );
+    if (response.data.msg == "user created successfully") {
+      navigate("/dashboard", { replace: true });
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/user/auth/",
+        {
+          email: username,
+          password,
+        }
+      );
+
+      const token = response.data.data;
+      if (!token) {
+        throw new Error("No token received from server");
+      }
+
+      localStorage.setItem("token", token);
+    }
+  };
   return (
     <div style={{ maxWidth: "400px", padding: "20px", margin: "0 auto" }}>
       <h2>Signup</h2>
@@ -10,6 +46,7 @@ const Signup = () => {
         <div style={{ marginBottom: "15px" }}>
           <input
             type="email"
+            ref={usernameRef}
             style={{
               width: "100%",
               padding: "10px",
@@ -24,6 +61,7 @@ const Signup = () => {
         <div style={{ marginBottom: "15px" }}>
           <input
             type="password"
+            ref={passwordRef}
             style={{
               width: "100%",
               padding: "10px",
